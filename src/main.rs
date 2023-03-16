15b9eec4ed212121172f3a4d1a2190aef2183df1
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2023-03-15 17:20:25 -0700

    Created subcommands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use clap::Command;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = Command::new("dm")
+        .version("1.0")
+        .about("Your personal directory door man")
+        .arg_required_else_help(true)
+        .subcommands([
+            Command::new("pwd").about("Prints current directory"),
+            Command::new("base").about("Prints base directory"),
+        ])
+        .get_matches();
+
+    match matches.subcommand() {
+        Some(("pwd", _)) => {
+            println!("{}", std::env::current_dir().unwrap().to_str().unwrap());
+        }
+        Some(("base", _)) => println!(
+            "{}",
+            std::env::current_dir()
+                .unwrap()
+                .to_str()
+                .unwrap()
+                .split('/')
+                .last()
+                .unwrap()
+        ),
+        _ => unreachable!(),
+    }
 }
